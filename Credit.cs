be4a479c0e21dfb9b0e9c345458fa97e2e0ffa6a
guney <guneykaya2002@gmail.com>
2021-12-12 00:32:17 +0300

    asdas

---
+++ Credit.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sipariş_Otomasyonu
+{
+    class Credit: Payment
+    {
+        public string Number { get; set; }
+        public string CardName { get; set; }
+        public string ExpDate { get; set; }
+        public int Cvv { get; set; }
+
+        public void Authorized(string cardName, string expDate, string number, int cvv)
+        {
+
+        }
+    }
+}
