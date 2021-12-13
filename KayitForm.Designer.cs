b3f98cee76a2b328d9bbd4fdbcd1e4d5990b36f0
barış oruç <barisoruc313@gmail.com>
2021-12-13 16:15:58 +0300

    login form kayıt form oluşturuldu sql database kullanıldı

---
+++ KayitForm.Designer.cs
@@ -0,0 +1,192 @@
+﻿
+namespace Sipariş_Otomasyonu
+{
+    partial class KayitForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnKayit = new System.Windows.Forms.Button();
+            this.btnGeri = new System.Windows.Forms.Button();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.txtSoyad = new System.Windows.Forms.TextBox();
+            this.txtid = new System.Windows.Forms.TextBox();
+            this.txtpw = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(86, 94);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ADINIZ:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(61, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "SOYADINIZ :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(43, 178);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "KULLANICI ADI :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(88, 219);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "ŞİFRE :";
+            // 
+            // btnKayit
+            // 
+            this.btnKayit.Location = new System.Drawing.Point(264, 242);
+            this.btnKayit.Name = "btnKayit";
+            this.btnKayit.Size = new System.Drawing.Size(75, 23);
+            this.btnKayit.TabIndex = 4;
+            this.btnKayit.Text = "KAYIT OL";
+            this.btnKayit.UseVisualStyleBackColor = true;
+            this.btnKayit.Click += new System.EventHandler(this.btnKayit_Click);
+            // 
+            // btnGeri
+            // 
+            this.btnGeri.Location = new System.Drawing.Point(264, 271);
+            this.btnGeri.Name = "btnGeri";
+            this.btnGeri.Size = new System.Drawing.Size(75, 23);
+            this.btnGeri.TabIndex = 5;
+            this.btnGeri.Text = "GERİ";
+            this.btnGeri.UseVisualStyleBackColor = true;
+            this.btnGeri.Click += new System.EventHandler(this.btnGeri_Click);
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(173, 91);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(166, 20);
+            this.txtAd.TabIndex = 6;
+            // 
+            // txtSoyad
+            // 
+            this.txtSoyad.Location = new System.Drawing.Point(173, 138);
+            this.txtSoyad.Name = "txtSoyad";
+            this.txtSoyad.Size = new System.Drawing.Size(166, 20);
+            this.txtSoyad.TabIndex = 7;
+            // 
+            // txtid
+            // 
+            this.txtid.Location = new System.Drawing.Point(173, 178);
+            this.txtid.Name = "txtid";
+            this.txtid.Size = new System.Drawing.Size(166, 20);
+            this.txtid.TabIndex = 8;
+            // 
+            // txtpw
+            // 
+            this.txtpw.Location = new System.Drawing.Point(173, 216);
+            this.txtpw.Name = "txtpw";
+            this.txtpw.Size = new System.Drawing.Size(166, 20);
+            this.txtpw.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(159, 32);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(189, 16);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "KULLANICI KAYIT EKRANI";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(182, 75);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(146, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Lütfen Bilgilerinizi Giriniz";
+            // 
+            // KayitForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(450, 338);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtpw);
+            this.Controls.Add(this.txtid);
+            this.Controls.Add(this.txtSoyad);
+            this.Controls.Add(this.txtAd);
+            this.Controls.Add(this.btnGeri);
+            this.Controls.Add(this.btnKayit);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "KayitForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kayıt Ekranı";
+            this.Load += new System.EventHandler(this.KayitForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnKayit;
+        private System.Windows.Forms.Button btnGeri;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.TextBox txtSoyad;
+        private System.Windows.Forms.TextBox txtid;
+        private System.Windows.Forms.TextBox txtpw;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
---
+++ LoginForm.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Sipariş_Otomasyonu
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+        string[] adminid = { "admin" };
+        string[] adminpw = { "1234" };
+
+        SqlConnection baglanti = new SqlConnection("Data Source=pcBaro;Initial Catalog=nesnekayit;Integrated Security=True");
+        private void btnKayitOl_Click(object sender, EventArgs e)
+        {
+            KayitForm kayt = new KayitForm();
+            kayt.Show();
+            this.Hide();
+
+        }
+
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGiris_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            string query = "Select *from tablekayit where KullaniciAdi = '" + txtKullaniciAdLogin.Text.Trim() + "' and Sifre = '" + txtSifreLogin.Text.Trim() + "'";
+            SqlDataAdapter sda = new SqlDataAdapter(query, baglanti);
+            DataTable dtbl = new DataTable();
+            sda.Fill(dtbl);
+            if(dtbl.Rows.Count==1)
+            {
+                Form2_Order_ frm2 = new Form2_Order_();
+                frm2.Show();
+                this.Hide();
+            }
+            else if (adminid.Contains(txtKullaniciAdLogin.Text) && adminpw.Contains(txtSifreLogin.Text) && Array.IndexOf(adminid, txtKullaniciAdLogin.Text) == Array.IndexOf(adminpw, txtSifreLogin.Text))
+            {
+
+            }
+            else
+            {
+                MessageBox.Show("Kullanıcı Adı ya da Şifreniz Yanlış");
+                txtKullaniciAdLogin.Text = string.Empty;
+                txtSifreLogin.Text = string.Empty;
+            }
+            baglanti.Close();
+
+
+        }
+    }
+}
