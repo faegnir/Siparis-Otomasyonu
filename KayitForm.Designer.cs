df0549117d03031b978ddca29acdf0583765c6ee
Faegnir <emre.senn97@gmail.com>
2021-12-25 19:50:08 +0300

    designed sign up

--- KayitForm.Designer.cs
+++ KayitForm.Designer.cs
@@ -45,57 +45,63 @@ namespace Sipariş_Otomasyonu
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(124, 115);
+            this.label1.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(62, 121);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(51, 17);
+            this.label1.Size = new System.Drawing.Size(51, 18);
             this.label1.TabIndex = 0;
             this.label1.Text = "NAME:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(95, 169);
+            this.label2.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(37, 167);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(80, 17);
+            this.label2.Size = new System.Drawing.Size(76, 18);
             this.label2.TabIndex = 1;
             this.label2.Text = "SURNAME:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(82, 218);
+            this.label3.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(27, 216);
             this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(93, 17);
+            this.label3.Size = new System.Drawing.Size(86, 18);
             this.label3.TabIndex = 2;
             this.label3.Text = "USER NAME:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(83, 265);
+            this.label4.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(30, 263);
             this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(92, 17);
+            this.label4.Size = new System.Drawing.Size(83, 18);
             this.label4.TabIndex = 3;
             this.label4.Text = "PASSWORD:";
             // 
             // btnKayit
             // 
-            this.btnKayit.Location = new System.Drawing.Point(352, 298);
+            this.btnKayit.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnKayit.Location = new System.Drawing.Point(287, 293);
             this.btnKayit.Margin = new System.Windows.Forms.Padding(4);
             this.btnKayit.Name = "btnKayit";
             this.btnKayit.Size = new System.Drawing.Size(100, 28);
             this.btnKayit.TabIndex = 4;
-            this.btnKayit.Text = "SIGN IN";
+            this.btnKayit.Text = "SIGN UP";
             this.btnKayit.UseVisualStyleBackColor = true;
             this.btnKayit.Click += new System.EventHandler(this.btnKayit_Click);
             // 
             // btnGeri
             // 
-            this.btnGeri.Location = new System.Drawing.Point(352, 334);
+            this.btnGeri.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnGeri.Location = new System.Drawing.Point(287, 329);
             this.btnGeri.Margin = new System.Windows.Forms.Padding(4);
             this.btnGeri.Name = "btnGeri";
             this.btnGeri.Size = new System.Drawing.Size(100, 28);
@@ -106,7 +112,7 @@ namespace Sipariş_Otomasyonu
             // 
             // txtAd
             // 
-            this.txtAd.Location = new System.Drawing.Point(231, 112);
+            this.txtAd.Location = new System.Drawing.Point(166, 119);
             this.txtAd.Margin = new System.Windows.Forms.Padding(4);
             this.txtAd.Name = "txtAd";
             this.txtAd.Size = new System.Drawing.Size(220, 22);
@@ -114,7 +120,7 @@ namespace Sipariş_Otomasyonu
             // 
             // txtSoyad
             // 
-            this.txtSoyad.Location = new System.Drawing.Point(231, 170);
+            this.txtSoyad.Location = new System.Drawing.Point(166, 165);
             this.txtSoyad.Margin = new System.Windows.Forms.Padding(4);
             this.txtSoyad.Name = "txtSoyad";
             this.txtSoyad.Size = new System.Drawing.Size(220, 22);
@@ -122,7 +128,7 @@ namespace Sipariş_Otomasyonu
             // 
             // txtid
             // 
-            this.txtid.Location = new System.Drawing.Point(231, 219);
+            this.txtid.Location = new System.Drawing.Point(166, 214);
             this.txtid.Margin = new System.Windows.Forms.Padding(4);
             this.txtid.Name = "txtid";
             this.txtid.Size = new System.Drawing.Size(220, 22);
@@ -130,7 +136,7 @@ namespace Sipariş_Otomasyonu
             // 
             // txtpw
             // 
-            this.txtpw.Location = new System.Drawing.Point(231, 266);
+            this.txtpw.Location = new System.Drawing.Point(166, 261);
             this.txtpw.Margin = new System.Windows.Forms.Padding(4);
             this.txtpw.Name = "txtpw";
             this.txtpw.Size = new System.Drawing.Size(220, 22);
@@ -140,19 +146,19 @@ namespace Sipariş_Otomasyonu
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label5.Location = new System.Drawing.Point(260, 32);
+            this.label5.Location = new System.Drawing.Point(171, 28);
             this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(77, 20);
+            this.label5.Size = new System.Drawing.Size(84, 20);
             this.label5.TabIndex = 10;
-            this.label5.Text = "SIGN IN";
+            this.label5.Text = "SIGN UP";
             this.label5.Click += new System.EventHandler(this.label5_Click);
             // 
             // KayitForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600, 416);
+            this.ClientSize = new System.Drawing.Size(450, 416);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.txtpw);
             this.Controls.Add(this.txtid);
